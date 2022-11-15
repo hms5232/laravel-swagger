212cddb6eee6593f92a7f218772328bc5c603a2d
hms5232 <43672033+hms5232@users.noreply.github.com>
2022-11-15 12:58:39 +0800

    feat: editor load index api file by default

--- tests/SwaggerEditorTest.php
+++ tests/SwaggerEditorTest.php
@@ -2,6 +2,8 @@
 
 namespace Hms5232\LaravelSwagger\Tests;
 
+use Illuminate\Support\Facades\File;
+
 class SwaggerEditorTest extends TestCase
 {
     /**
@@ -31,6 +33,21 @@ class SwaggerEditorTest extends TestCase
     }
 
     /**
+     * Define environment setup for enabling Laravel Swagger,
+     * move openapi file and custom index of openapi file.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function swaggerCustomIndexFile($app)
+    {
+        $this->enableLS($app);
+        $this->copyFixtures();
+        $app['config']->set('swagger.index', 'index.yaml');
+        File::move(storage_path('swagger/openapi.yaml'), storage_path('swagger/index.yaml'));
+    }
+
+    /**
      * @test
      * @define-env disableLS
      */
@@ -45,7 +62,9 @@ class SwaggerEditorTest extends TestCase
      */
     public function testDefaultRoute()
     {
-        $this->get('/swagger-editor')->assertStatus(200);
+        $res = $this->get('/swagger-editor');
+        $res->assertStatus(200);
+        $res->assertSee('http://localhost/swagger-doc/openapi.yaml');
     }
 
     /**
@@ -65,5 +84,19 @@ class SwaggerEditorTest extends TestCase
     public function testNullPath()
     {
         $this->get('/swagger-editor')->assertStatus(404);
+    }
+
+    /**
+     * @test
+     * @define-env swaggerCustomIndexFile
+     */
+    public function testCustomIndexFile()
+    {
+        $this->assertFileDoesNotExist(storage_path('swagger/openapi.yaml'));
+        $this->get('/swagger-doc/index.yaml')->assertStatus(200);
+
+        $res = $this->get('/swagger-editor');
+        $res->assertDontSee('Failed to load API definition.');
+        $res->assertSee('http://localhost/swagger-doc/index.yaml');
     }
 }
